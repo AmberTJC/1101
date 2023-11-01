d22b30a6b3ab69e9a47aae60879a3cac2d2e4242
Amber <amber.campbell@edu.sait.ca>
2023-11-01 15:33:09 -0600

    identify min max

---
+++ main.py
@@ -0,0 +1,20 @@
+data = open("one_hundred.txt")
+max_value = ""
+min_value = "99999"
+for line in data:
+    if len(line) > 1:
+       list_nums = line.split()
+       for num in list_nums:
+           if num < min_value:
+               min_value = num
+           if num > max_value:
+                 max_value = num 
+print (f"{min_value}, {max_value}") 
+data.seek(0)
+for line in data:
+    if len(line) > 1:
+       list_nums = line.split()
+       for num in list_nums:
+           for r in range(int(min_value), int(max_value)):
+               if r != num:
+                   print (f"{r} is not present")
