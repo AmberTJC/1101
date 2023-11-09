62d0453822acb5437d0edaa5a01dc5cf8c07a71c
Amber <amber.campbell@edu.sait.ca>
2023-11-09 13:16:38 -0700

    sorted

--- main.py
+++ main.py
@@ -1,20 +1,23 @@
-data = open("one_hundred.txt")
-max_value = ""
-min_value = "99999"
-for line in data:
-    if len(line) > 1:
-       list_nums = line.split()
-       for num in list_nums:
-           if num < min_value:
-               min_value = num
-           if num > max_value:
-                 max_value = num 
-print (f"{min_value}, {max_value}") 
-data.seek(0)
-for line in data:
-    if len(line) > 1:
-       list_nums = line.split()
-       for num in list_nums:
-           for r in range(int(min_value), int(max_value)):
-               if r != num:
-                   print (f"{r} is not present")
+def bubble_sort (master_list):
+    length = len(master_list)
+    for i in range (length):
+        for j in range (0, length - i - 1):
+            if master_list [j] > master_list [j + 1]:
+                master_list[j], master_list [j + 1] = master_list [j + 1], master_list [j]
+
+
+with open ("one_hundred.txt") as data:
+    master_list = []
+    for line in data:
+        if len(line) >1:
+            list_nums = line.split()
+            list_nums = [int (i) for i in list_nums]
+            master_list.extend(list_nums)
+#print (master_list)
+    for r in range (100):
+        if r not in master_list:
+            print (f"{r} is not present")
+
+bubble_sort (master_list)
+with open ("one_hundred_sorted.txt", "w") as f:
+    f.write (str(master_list))
